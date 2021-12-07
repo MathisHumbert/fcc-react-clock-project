efd0ee6ec1a5de748307b3bca278c1737f64c30b
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-07 15:17:44 +0100

    react almost done

--- src/index.js
+++ src/index.js
@@ -5,38 +5,65 @@ import './index.css';
 let coutdown;
 
 const App = () => {
+  // session time
   const [time, setTime] = useState(1500);
   const [timeLength, setTimeLength] = useState(25);
+
+  // break time
   const [breakTime, setBreakTime] = useState(300);
-  const [displayBreakTime, setDiplayBreakTime] = useState(5);
+  const [breakTimeLength, setBreakTimeLength] = useState(5);
+
+  // flags
   const [start, setStart] = useState(false);
+  const [flagTime, setFlagTime] = useState(true);
   const mounted = useRef(false);
 
+  // timer function
   const timer = (seconds) => {
+    // clear any interval
     clearInterval(coutdown);
     const now = Date.now();
     const then = now + seconds * 1000;
     displayTimeLeft(seconds);
 
     coutdown = setInterval(() => {
+      // pause
       if (!start) {
         clearInterval(coutdown);
         return;
       }
+
       const secondsLeft = Math.round((then - Date.now()) / 1000);
+
+      // if time is zero
       if (secondsLeft < 0) {
         clearInterval(coutdown);
-        // call break time
+        // set session time back to start and launch break time
+        if (flagTime) {
+          setTime(timeLength * 60);
+          setFlagTime(false);
+        }
+        // set break time back to start and launch session time
+        else {
+          setBreakTime(breakTimeLength * 60);
+          setFlagTime(true);
+        }
         return;
       }
-      setTime(secondsLeft);
+
+      // toggle the time
+      if (flagTime) {
+        setTime(secondsLeft);
+      } else {
+        setBreakTime(secondsLeft);
+      }
     }, 1000);
   };
 
   const displayTimeLeft = (seconds) => {
     const minutes = Math.floor(seconds / 60);
     const remainderSeconds = seconds % 60;
-    const display = `${minutes < 10 ? `0${minutes}` : minutes} : ${
+    const display = `${minutes < 10 ? `0${minutes}` : minutes}:${
       remainderSeconds < 10 ? `0${remainderSeconds}` : remainderSeconds
     }`;
     return display;
@@ -44,6 +71,15 @@ const App = () => {
 
   const handelStart = () => {
     setStart((oldValue) => !oldValue);
+  };
+
+  const resetTime = () => {
+    setTime(1500);
+    setTimeLength(25);
+    setBreakTime(300);
+    setBreakTimeLength(5);
+    setStart(false);
+    setFlagTime(true);
   };
 
   const toggleTime = (value) => {
@@ -68,10 +104,26 @@ const App = () => {
     }
   };
 
-  const resetTime = () => {
-    setTime(1500);
-    setTimeLength(25);
-    setStart(false);
+  const toggleBreakTime = (value) => {
+    if (value === 'inc') {
+      setBreakTimeLength((oldValue) => {
+        let newValue = oldValue + 1;
+        if (oldValue >= 60) {
+          newValue = 60;
+        }
+        setBreakTime(newValue * 60);
+        return newValue;
+      });
+    } else {
+      setBreakTimeLength((oldValue) => {
+        let newValue = oldValue - 1;
+        if (oldValue <= 1) {
+          newValue = 1;
+        }
+        setBreakTime(newValue * 60);
+        return newValue;
+      });
+    }
   };
 
   useEffect(() => {
@@ -79,37 +131,61 @@ const App = () => {
       mounted.current = true;
       return;
     }
-    timer(time);
-  }, [start]);
+    if (flagTime) {
+      timer(time);
+    } else {
+      timer(breakTime);
+    }
+  }, [start, flagTime]);
 
   return (
     <main>
       <h1 className="title">FCC podomoro</h1>
-      <Controlers toggleTime={toggleTime} timeLength={timeLength} />
+      <Controlers
+        toggleTime={toggleTime}
+        timeLength={timeLength}
+        toggleBreakTime={toggleBreakTime}
+        breakTimeLength={breakTimeLength}
+      />
       <Clock
         handelStart={handelStart}
         start={start}
         time={time}
+        breakTime={breakTime}
         displayTimeLeft={displayTimeLeft}
         resetTime={resetTime}
+        flagTime={flagTime}
       />
     </main>
   );
 };
 
-const Controlers = ({ toggleTime, timeLength }) => {
+const Controlers = ({
+  toggleTime,
+  timeLength,
+  toggleBreakTime,
+  breakTimeLength,
+}) => {
   return (
     <header className="controlers">
       <div className="controlers-break">
         <h2 id="break-label">Break Length</h2>
         <div className="time-btn-container">
-          <button id="break-decrement" className="time-btn">
+          <button
+            id="break-decrement"
+            className="time-btn"
+            onClick={() => toggleBreakTime('dec')}
+          >
             -
           </button>
           <h3 id="break-length" className="controlers-time">
-            5
+            {breakTimeLength}
           </h3>
-          <button id="break-increment" className="time-btn">
+          <button
+            id="break-increment"
+            className="time-btn"
+            onClick={() => toggleBreakTime('inc')}
+          >
             +
           </button>
         </div>
@@ -140,16 +216,35 @@ const Controlers = ({ toggleTime, timeLength }) => {
   );
 };
 
-const Clock = ({ handelStart, start, time, displayTimeLeft, resetTime }) => {
+const Clock = ({
+  handelStart,
+  start,
+  time,
+  displayTimeLeft,
+  resetTime,
+  breakTime,
+  flagTime,
+}) => {
   return (
     <section className="clock">
       <div className="clock-container">
-        <h2 id="time-left" className="clock-title">
-          Session
+        <h2 id="timer-label" className="clock-title">
+          {flagTime ? 'Session' : 'Break'}
         </h2>
         <div className="clock-container">
-          <h1 id="time-left" className="clock-container-time">
-            {displayTimeLeft(time)}
+          <h1
+            id="time-left"
+            className={`clock-container-time ${
+              flagTime
+                ? time < 60
+                  ? 'active'
+                  : null
+                : breakTime < 60
+                ? 'active'
+                : null
+            }`}
+          >
+            {flagTime ? displayTimeLeft(time) : displayTimeLeft(breakTime)}
           </h1>
         </div>
         <div className="clock-btn-container">
