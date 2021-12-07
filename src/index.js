2948f6a871506bfdb95bd6a4bb4491e59dc8c438
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2021-12-07 13:11:44 +0100

    jsx and css almost done

--- src/index.js
+++ src/index.js
@@ -1,8 +1,80 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const App = () => {
+  const [time, setTime] = useState(25);
+  const [breakTime, setBreakTime] = useState(5);
+  const [start, setStart] = useState(false);
+
+  return (
+    <main>
+      <h1 className="title">FCC podomoro</h1>
+      <Controlers />
+      <Clock />
+    </main>
+  );
+};
+
+const Controlers = () => {
+  return (
+    <header className="controlers">
+      <div className="controlers-break">
+        <h2 id="break-label">Break Length</h2>
+        <div className="time-btn-container">
+          <button id="break-decrement" className="time-btn">
+            -
+          </button>
+          <h3 id="break-length" className="controlers-time">
+            5
+          </h3>
+          <button id="break-increment" className="time-btn">
+            +
+          </button>
+        </div>
+      </div>
+      <div className="controlers-session">
+        <h2 id="session-label">Session Length</h2>
+        <div className="time-btn-container">
+          <button id="session-decrement" className="time-btn">
+            -
+          </button>
+          <h3 id="session-length" className="controlers-time">
+            25
+          </h3>
+          <button id="session-increment" className="time-btn">
+            +
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+const Clock = () => {
+  return (
+    <section className="clock">
+      <div className="clock-container">
+        <h2 id="time-left" className="clock-title">
+          Session
+        </h2>
+        <div className="clock-container">
+          <h1 id="time-left" className="clock-container-time">
+            25:00
+          </h1>
+        </div>
+        <div className="clock-btn-container">
+          <button id="start_stop" className="start-stop-btn">
+            start
+          </button>
+          <button id="reset" className="reset-btn">
+            reset
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +82,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
